61e4e20a46ffcffcb0c318d1427f7669f1800fd2
peiyu11211225 <peiyujian752@gmail.com>
2026-05-28 23:31:09 +0800

    fix system packages for python 3.11

--- processor.py
+++ processor.py
@@ -228,7 +228,7 @@ class PoseProcessor:
  
         out = cv2.VideoWriter(
             output_path,
-            cv2.VideoWriter_fourcc(*'mp4v'),
+            cv2.VideoWriter_fourcc(*'avc1'),
             fps,
             (w, h)
         )
