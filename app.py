a091c6f5a4ca374389ad2c094245689188731629
peiyu11211225 <peiyujian752@gmail.com>
2026-06-10 00:03:56 +0800

    fix python version

--- app.py
+++ app.py
@@ -48,11 +48,8 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     if st.button("🏹\n高遠球\n正拍"):
-        st.switch_page("ai_coach")
-        st.switch_page("analyzer")
-        st.switch_page("processor")
-        st.switch_page("record_stander")
-        
+        st.switch_page("pages/analyzer.py")
+
 with col2:
     if st.button("🔄\n高遠球\n反拍"):
         st.switch_page("pages/2_overhead_clear_bh.py")
