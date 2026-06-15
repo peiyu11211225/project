1d0cadd63aeb59052c3cf92b7a6494045d73ad48
peiyu11211225 <peiyujian752@gmail.com>
2026-06-15 18:41:01 +0800

    fix python version

--- pages/1_analyzer.py
+++ pages/1_analyzer.py
@@ -141,7 +141,7 @@ def show_help_dialog():
     """
     
     # 渲染 HTML 元件 (高度可以根據你的影片比例調整，這裡抓 450 像素)
-    components.html(html_code, height=450)
+    components.html(html_code, height=250)
 
     st.write("---")
     if st.button("關閉"):
