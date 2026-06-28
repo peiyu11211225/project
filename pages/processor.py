6c7c470a8c95c221a724b83717058605f21420cd
peiyu11211225 <peiyujian752@gmail.com>
2026-06-28 20:52:11 +0800

    fix python version

--- pages/processor.py
+++ pages/processor.py
@@ -6,7 +6,7 @@ import pandas as pd
 from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 from ai_coach import AICoach
-from pagesa.pose_utils import get_full_body_angles
+from pages.pose_utils import get_full_body_angles
 from ai_coach import AICoach
 
 class PoseProcessor:
